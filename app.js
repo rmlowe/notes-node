5219398efe02ffad295ad7aa0c56d3f6246abf45
Robert Lowe <robertmlowe@rmlowe.com>
2018-08-03 14:32:48 +0800

    Using 3rd Party Modules

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-console.log('Result:', notes.add(9, -2));
+// console.log(_.isString(true));
+// console.log(_.isString('Andrew'));
+var filteredArray = _.uniq(['Andrew', 1, 'Andrew', 1, 2, 3, 4]);
+console.log(filteredArray);
+
+// console.log('Result:', notes.add(9, -2));
 
 // var user = os.userInfo();
 //
